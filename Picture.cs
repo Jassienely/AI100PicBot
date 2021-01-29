b1dbc903abcab0272f043e894afa07807ccc4ac8
jassienely <jasmine.nely@gmail.com>
2021-01-29 16:24:18 +0800

    create class for picture object from query

---
+++ Picture.cs
@@ -0,0 +1,8 @@
+namespace PictureBot.Bots{
+    public class Picture
+    {
+        public string Name { get; set; }
+        public string Url { get; set; }
+        public string Description { get; set; }
+    }
+}
